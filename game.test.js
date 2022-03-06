1aeacda0a62a14d1b67d16d0b7140cb385a079dc
ynnoj2 <74754775+ynnoj2@users.noreply.github.com>
2022-03-06 22:51:00 +1100

    Tests for validFrameAmount and moveFrame

--- game.test.js
+++ game.test.js
@@ -3,7 +3,6 @@ const { Game } = require('./game');
 //test function is a part of the jest library and is automatically added to .test.js files
 
 test('roll adds value to bowlingGame.match array', () => {
-  // Prepare a spy for the Game.roll method
   const noOfPins = 0;
   const bowlingGame = new Game();
   const spy = jest.spyOn(bowlingGame, 'roll');
@@ -15,7 +14,6 @@ test('roll adds value to bowlingGame.match array', () => {
 });
 
 test('validRollAmount will be false when noOfPins =11', () => {
-  // Prepare a spy for the Game.roll method
   const noOfPins = 11;
   const bowlingGame = new Game();
   const spy = jest.spyOn(bowlingGame, 'validRollAmount');
@@ -26,7 +24,6 @@ test('validRollAmount will be false when noOfPins =11', () => {
 });
 
 test('validRollAmount will be false when noOfPins =-1', () => {
-  // Prepare a spy for the Game.roll method
   const noOfPins = -1;
   const bowlingGame = new Game();
   const spy = jest.spyOn(bowlingGame, 'validRollAmount');
@@ -37,12 +34,55 @@ test('validRollAmount will be false when noOfPins =-1', () => {
 });
 
 test('validRollAmount will be true when noOfPins =1', () => {
-  // Prepare a spy for the Game.roll method
   const noOfPins = 1;
   const bowlingGame = new Game();
   const spy = jest.spyOn(bowlingGame, 'validRollAmount');
   const result = bowlingGame.validRollAmount(noOfPins);
   expect(spy).toHaveBeenCalled();
   expect(result).toBe(true);
+  spy.mockRestore();
+});
+
+test('validFrameAmount will return false if 2,9 rolled ', () => {
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'validFrameAmount');
+  bowlingGame.roll(2);
+  const result = bowlingGame.validFrameAmount(9);
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(false);
+  spy.mockRestore();
+});
+
+test('validFrameAmount will return true if 1, 2,9 rolled ', () => {
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'validFrameAmount');
+  bowlingGame.roll(1);
+  bowlingGame.roll(2);
+  const result = bowlingGame.validFrameAmount(9);
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(true);
+  spy.mockRestore();
+});
+
+//TODO test move frame
+test('moveFrame cause attempt to become 2 if current attempt is 1 ', () => {
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'moveFrame');
+  bowlingGame.attempt = 1;
+  bowlingGame.moveFrame();
+  const result = bowlingGame.attempt;
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(2);
+  spy.mockRestore();
+});
+
+test('moveFrame cause attempt to become 1 if current attempt is 2 ', () => {
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'moveFrame');
+  bowlingGame.attempt = 2;
+  bowlingGame.moveFrame();
+  const result = bowlingGame.attempt;
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(1);
   spy.mockRestore();
 });
