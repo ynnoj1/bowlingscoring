f07772704850a0f2962cf6cf7a4eb84e7849a8f3
ynnoj2 <74754775+ynnoj2@users.noreply.github.com>
2022-03-06 21:17:45 +1100

    validRollAmount to check noOfPins passed to roll

--- game.js
+++ game.js
@@ -5,5 +5,9 @@ module.exports = {
     this.roll = function roll(noOfPins) {
       this.match = [...this.match, noOfPins];
     };
+    this.validRollAmount = function validRollAmount(noOfPins) {
+      let validRollAmounts = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      return validRollAmounts.includes(noOfPins) ? true : false;
+    };
   },
 };
