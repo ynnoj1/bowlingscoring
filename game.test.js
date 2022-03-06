7dca7a393f5d2dbe51799f776b67be5239e243e4
ynnoj2 <74754775+ynnoj2@users.noreply.github.com>
2022-03-06 21:18:30 +1100

    Tests of 11 and -1 for validRollAmount

--- game.test.js
+++ game.test.js
@@ -13,3 +13,25 @@ test('roll adds value to bowlingGame.match array', () => {
   expect(result).toBe(noOfPins);
   spy.mockRestore();
 });
+
+test('validRollAmount will be false when noOfPins =11', () => {
+  // Prepare a spy for the Game.roll method
+  const noOfPins = 11;
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'validRollAmount');
+  const result = bowlingGame.validRollAmount(noOfPins);
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(false);
+  spy.mockRestore();
+});
+
+test('validRollAmount will be false when noOfPins =-1', () => {
+  // Prepare a spy for the Game.roll method
+  const noOfPins = -1;
+  const bowlingGame = new Game();
+  const spy = jest.spyOn(bowlingGame, 'validRollAmount');
+  const result = bowlingGame.validRollAmount(noOfPins);
+  expect(spy).toHaveBeenCalled();
+  expect(result).toBe(false);
+  spy.mockRestore();
+});
