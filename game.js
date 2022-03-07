891c480f5e692bbee9610009f4749dc6c2299f1c
ynnoj2 <74754775+ynnoj2@users.noreply.github.com>
2022-03-07 12:28:08 +1100

    Strike and frame logic updated frameHistory property added to store the frame for each roll moveFrame method  now take noOfPins argument moveFrame attempt and frame upon strike remainingPins returns 10 for a strike

--- game.js
+++ game.js
@@ -3,18 +3,21 @@ module.exports = {
     this.player = player;
     this.match = [];
     this.frame = 1;
+    this.frameHistory = [];
     this.attempt = 1;
     this.score = 0;
     this.roll = function roll(noOfPins) {
-      if (!this.validRollAmount(noOfPins) || !this.validFrameAmount(noOfPins)) {
+      if (!this.validFrameAmount(noOfPins)) {
+        console.log(`${noOfPins} exceeeds frame limit`);
         return;
       }
-      this.match = [...this.match, noOfPins];
-      this.moveFrame();
-      if (this.strike(noOfPins)) {
-        //reset the attempt after a strike
-        this.attempt = 1;
+      this.match = [...this.match, noOfPins]; //store the number of pins rolled
+      this.frameHistory = [...this.frameHistory, this.frame]; // store the current frame
+      if (!this.validRollAmount(noOfPins)) {
+        console.warn(`${noOfPins} is not a valid noOfPins`);
+        return;
       }
+      this.moveFrame(noOfPins);
       this.updateScore();
     };
     this.validRollAmount = function validRollAmount(noOfPins) {
@@ -22,18 +25,22 @@ module.exports = {
       return validRollAmounts.includes(noOfPins) ? true : false;
     };
     this.validFrameAmount = function validFrameAmount(noOfPins) {
-      if (this.match.length < 1 || this.attempt == 1) {
+      if (this.attempt == 1) {
         //no need to check the frame amount on the first roll or attempt
         return true;
       }
       const remainingPins = this.remainingPins(noOfPins);
       return remainingPins < 0 ? false : true;
     };
-    this.moveFrame = function moveFrame() {
+    this.moveFrame = function moveFrame(noOfPins) {
       if (this.attempt == 2) {
+        //we are at the end of the frame
         this.frame++;
         this.attempt = 1;
         this.updateScore();
+      } else if (this.strike(noOfPins)) {
+        this.attempt = 1;
+        this.frame++;
       } else {
         this.attempt++;
       }
@@ -46,8 +53,12 @@ module.exports = {
       return remainingPins == 0 && this.attempt == 2 ? true : false;
     };
     this.remainingPins = function remainingPins(noOfPins) {
+      //must be called before the number of pins is added to match
       const lastRoll = this.match.at(-1);
       const remainingPins = 10 - lastRoll - noOfPins;
+      if (this.strike(noOfPins)) {
+        return 10;
+      }
       return remainingPins;
     };
     this.updateScore = function updateScore() {
