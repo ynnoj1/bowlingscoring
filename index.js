85403d9db271cc472cdb321144169104049d5fb1
ynnoj2 <74754775+ynnoj2@users.noreply.github.com>
2022-03-08 21:44:36 +1100

    Examples of interacting with the game

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { Game } = require('./game');
 
-const bowlingGame = new Game();
-bowlingGame.roll(6);
+const bowlingGame = new Game('Player Name');
+bowllingGame.roll(4);
+bowlingGame.rollMany(1, 2, 3);
+bowlingGame.score();
+bowlingGame.scoreboard();
